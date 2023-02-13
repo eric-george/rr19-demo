0e8912f0cabd7c1dca8cfcce3ce3530997969251
Eric George <george.bardo5@gmail.com>
2023-02-13 14:09:00 -0700

    Initial Project Checkin

---
+++ src/main/java/com/rr19/egeorgedemo/Rr19Demo.java
@@ -0,0 +1,12 @@
+package com.rr19.egeorgedemo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+//@SpringBootApplication
+public class Rr19Demo {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(Rr19DemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rr19/egeorgedemo/Rr19DemoTests.java
@@ -0,0 +1,15 @@
+package com.rr19.egeorgedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+class Rr19DemoTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(10 == 10);
+    }
+}
